c86a38fad2b4415b3f7c40ea4f440d965d6f8f08
Johnathon Wood <jaywood128@gmail.com>
2018-09-13 14:16:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function currentLine(line)  {
     var linesNamesandNumbers = [];
 
     for(var i = 0; i<line.length; i++) {
-       lineNamesandNumbers.push(i +1 + "." + line[i]);
+       linesNamesandNumbers.push(i +1 + "." + line[i]);
      }
      console.log("The line is currently: " +linesNamesandNumbers)
      return "The line is currently: " + linesNamesandNumbers.join(",");
