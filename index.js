6dbcb7e50e03abcde4b71acf181e63436f8f92e7
Johnathon Wood <jaywood128@gmail.com>
2018-09-13 14:07:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function nowServing(line) {
   return "Currently serving " + line.shift() + ".";
   }
 }
+function currentLine(line)  { 
+
+    if (!line.length)  {
+        return "The line is currently empty."
+    }
+    var linesNamesandNumbers = [];
+
+    for(var i = 0; i<line.length; i++) {
+       lineNamesandNumbers.push(i +1 + "." + line[i]); 
+     }
+     console.log("The line is currently: " +linenamesandNumbers)
+     return "The line is currently: " + linenamesandNumbers.join(",")
+}
